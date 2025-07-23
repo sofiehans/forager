f32a74ce3cc20ac7db1c1a66c4aaa05dc7b40ed9
Sophie Hans <s.hans@simfront.com>
2025-07-22 21:21:00 -0400

    create MainScreen and redirect index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,76 +1,8 @@
-import Button from '@/components/Button';
-import { Text, View, StyleSheet, Alert } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-// import CameraComponent from '@/components/CameraComponent';
-import { SourceTextModule } from 'vm';
-import { Link, useRouter } from 'expo-router';
+import React from 'react';
+import { registerRootComponent } from 'expo';
+import MainScreen from './MainScreen';
 
-export default function Index() {
-  const router = useRouter();
+const App = () => <MainScreen />;
 
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      try {
-        const hasLaunched = await AsyncStorage.getItem('hasLaunched');
-        if (!hasLaunched) {
-          // First launch
-          Alert.alert('Forager should not be used to identify mushrooms for consumption - always be aware that mushrooms can be harmful if consumed. Forager is not liable for any injuries that occur.');
-          await AsyncStorage.setItem('hasLaunched', 'true');
-        }
-      } catch (e) {
-        console.error('Error checking first launch', e);
-      }
-    };
-
-    checkFirstLaunch();
-  }, []);
-
-  //REMOVE AFTER DEVOLOPMENT
-  const resetLaunchFlag = async () => {
-    await AsyncStorage.removeItem('hasLaunched');
-    alert('Launch flag reset — restart app to test!');
-  };
-
-  const identify = () => {
-    router.push('/CameraScreen');
-  }
-
-  const history = () => {
-    //TODO: Implement history viewing
-    Alert.alert(
-      'Not Available',
-      'This feature has not been implmented yet! Come back later!'
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <View >
-        <Button imageSource={require('../assets/images/identify.png')} onPress={identify} style={{ marginBottom: 30 }} />
-        <Button imageSource={require('../assets/images/history.png')} onPress={history} />
-
-        {/* REMOVE AFTER DEVOLOPMENT */}
-        <View style={{ marginTop: 20 }}>
-          <Button onPress={resetLaunchFlag} style={{ width: 60, height: 60, backgroundColor: '#FF0000' }} />
-        </View>
-        {/* ################# */}
-
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1D3836',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 40,
-  },
-  text: {
-    color: '#fff',
-  },
-});
-
+registerRootComponent(App);
+export default App;
